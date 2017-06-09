35e54027dc5c97c5684a3f0446b4f4f3ea1cf245
triyanarief <insanmedika@icloud.com>
2017-06-09 14:56:51 +0700

    test route

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var passportLocalMongoose = require('passport-local-mongoose');
+// var passportLocalMongoose = require('passport-local-mongoose');
 
 var UserSchema = new mongoose.Schema({
   username: String,
@@ -9,5 +9,5 @@ var UserSchema = new mongoose.Schema({
   retypePassword: String
 });
 
-UserSchema.plugin(passportLocalMongoose);
+// UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model('User', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var user = require('../models/user.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', (req, res) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var phoneNumber = req.body.phoneNumber;
+  var password = req.body.password;
+  var retypePassword = req.body.retypePassword;
+  var newUser = {name:name, email:email, phoneNumber:phoneNumber, password:password, retypePassword:retypePassword};
+  user.create(newUser, (err, newlyCreated) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
+
 });
 
 module.exports = router;
