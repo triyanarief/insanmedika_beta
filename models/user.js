b42ea6316ac292521e7d8c32799ed4c3e2ef64bf
triyanarief <insanmedika@icloud.com>
2017-06-09 11:18:58 +0700

    first skeleton web

---
+++ models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var UserSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  phoneNumber: Number,
+  password: String,
+  retypePassword: String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', UserSchema);
